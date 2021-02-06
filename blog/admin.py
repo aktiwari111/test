0be65f6f2086e7ff26c1b1f9f4281ba9148c480c
aktiwari111 <studenttraining111@gmail.com>
2021-02-06 14:56:07 +0530

    2 commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+admin.site.site_title = "Hind Gyanodaya"
+admin.site.site_header = "Hind Gyanodaya Higher Secondary School Rewa (M.P.)"
+admin.site.index_title = "Hind Gyanodaya Administration"
+#  administration
---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    profile = models.ImageField(
+        default='default.jpg', upload_to='profile_pics')
+    title = models.CharField(max_length=100)
+    content = models.TextField(null=True, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.URLField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='profile_pics/')
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+    
